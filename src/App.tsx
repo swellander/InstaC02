fea8e409891c7330a5b8cdcda297802ffd2aa65a
Sam Wellander <samwellander@gmail.com>
2019-03-09 17:26:25 -0500

    add bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+// import Header from "./components/header/header";
 
-export default class extends React.Component {
+export default class App extends React.Component {
   render() {
-    return <h1>Working</h1>;
+    const { btnText, button, container, content } = styles;
+    return (
+      <View style={container}>
+        <ImageBackground
+          style={{ width: "100%", height: "100%" }}
+          source={require("../login_page_bg.png")}
+        >
+          <View style={content}>
+            <TouchableOpacity
+              style={button}
+              onPress={() => alert("Sams alert")}
+            >
+              <Text style={btnText}>Analyze</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eee",
+    justifyContent: "center"
+  },
+  button: {
+    display: "flex",
+    borderRadius: 15,
+    width: 100,
+    height: 40,
+    color: "white",
+    backgroundColor: "#5162ce",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  btnText: {
+    color: "white"
+  },
+  header: {
+    flex: 1
+  },
+  content: {
+    flex: 6,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
